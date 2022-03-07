d9a8d26d4f049790812a9cd7c410d4d24baaeefd
exam293 <101106166+exam293@users.noreply.github.com>
2022-03-07 14:11:26 +0000

    homepage

--- Homepage.php
+++ Homepage.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="Webstyle.css">
     <title>GibJohn</title>
 </head>
 <body>
@@ -19,7 +22,7 @@
         <div class="collapse navbar-collapse" id="mynavbar">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
+                    <a class="nav-link" href="javascript:void(0)">Contact Us</a>
                 </li>
             </ul>
             <form class="d-flex" method="get" acton="/projects/Tutor_student.php">
