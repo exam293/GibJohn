57ab953e01177ceebb31815a9989de7bac48889d
exam293 <101106166+exam293@users.noreply.github.com>
2022-03-07 13:51:10 +0000

    Create Homepage.php

---
+++ Homepage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GibJohn</title>
+</head>
+<body>
+    
+<nav class="navbar navbat-expand-sm grey">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="logo.png" alt="Company logo" style="width:99px; height:60px">
+        </a> 
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+            <form class="d-flex" method="get" acton="/projects/Tutor_student.php">
+                <button class="btn btn-primary" type="submit">Login</button>
+            </form>
+       
+        </div>
+    </div>
+</nav> 
+</body>
+</html>
