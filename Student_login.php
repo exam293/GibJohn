46c641b69e82a73086959000e5e939ee55b3b0a5
exam293 <101106166+exam293@users.noreply.github.com>
2022-03-08 14:30:35 +0000

    Links activated and student login development
    active links to all of the current pages(Contact, Homepage, tutor or student, student login, students login, students sign-up, tutor login )

--- Student_login.php
+++ Student_login.php
@@ -1,23 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="webstyle.css">
     <title>Document</title>
-<script>
-function Student_link()
-{
-     location.href = "Student_login.php";
-} 
-function Tutor_link()
-{
-     location.href = "Tutor_login.php";
-} 
-</script>
 </head>
 <body>
     
@@ -32,7 +22,7 @@ function Tutor_link()
         <div class="collapse navbar-collapse" id="mynavbar">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0)">Contact Us</a>
+                    <a class="nav-link" href="Contact.php">Contact Us</a>
                 </li>
             </ul>
             <form class="d-flex" method="get" action="/Gibjohn/Tutor_student.php">
@@ -43,14 +33,26 @@ function Tutor_link()
     </div>
 </nav> 
 
-<div class="container mt-3">
-  <center>
-    <h2>Hello, are you a student or tutor?</h2>
-  </center>
-  <div class="d-grid gap-3">
-    <button class="btn btn-primary btn-block" type="button" onclick="Student_link()">Student</button>
-    <button class="btn btn-primary btn-block" type="button" onclick="Tutor_link()">Tutor</button>
-  </div>
+<div style="padding: 50px; margin-top: 3em">
+    <h1>Log In</h1>
+    <form action="Student_login_action.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</lable>
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</lable>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        <div>
+            <a>New student?</a><a href="Student_sign-up.php" style="text-decoration: none;"> Start here.</a>
+        </div>
+        <div>
+            <a>If you are a tutor?</a><a href="Tutor_login.php" style="text-decoration: none;"> Click here.</a>
+        </div>
+
+    </form>
 </div>
 
 </body>
---
+++ User.php
@@ -0,0 +1,63 @@
+<?php
+
+class user {
+    public $email = "";
+    public $password = "";
+    public $password_hash = "";
+    public $authenticated = false;
+    private $connection;
+
+    function __construct($connection, $email, $password) {
+        $this->email = mysqli_real_escape_string($connection, $email);
+        $this->password = mysqli_real_escape_string($connection, $password);
+
+        $this->password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $this->connection = $connection;
+
+    }
+
+    
+    #function insert() {
+     #   $sql = "
+      #  Insert INTO users(
+       #     email,
+        #    password,
+         #   is_active
+#        )
+ #       VALUES (
+  #          '{$this->email}',
+   #         '{$this->password_hash}',
+    #        '0'
+     #   )    
+      #  ";
+        
+       # $sqlQuery = mysqli_query($this->connection, $sql);
+
+#        if(!$sqlQuery){
+#
+ #           die("MySQL query failed!" . mysqli_error($this->connection));
+  #      }
+#
+ #   }
+##
+   function authenticate() {
+       $sql = "
+          SELECT Student_Id, First_name, Last_name, Email, Password, DOB, Start_date, Status
+            FROM student
+            WHERE Email=\"{$this->email}\";
+            ";
+        
+            $result = $this->connection->query($sql);
+            if ($row = $result->fetch_assoc()) {
+
+                if(password_verify($this->password, $row['Password'])) {
+                    $this->authenticated = true;
+                }
+            }
+    }
+
+    function is_logged_in(){
+        return $this->authenticated;
+    }
+}
